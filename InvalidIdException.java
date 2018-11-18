87f2010bb66f999170405d92acac52d01c1077b3
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-17 20:46:03 -0500

    New Exception for Improperly Formatted IDs

---
+++ InvalidIdException.java
@@ -0,0 +1,9 @@
+
+/**
+ * Thrown when the id is invalidly formatted
+ */
+public class InvalidIdException extends Exception{
+    public InvalidIdException(String id) {
+        super(id + " is an invalid student ID");
+    }
+}
