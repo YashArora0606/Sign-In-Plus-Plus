353aa6fd670f0f6b8437716de0bc94b85194d3b3
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-17 20:14:27 -0500

    Put Grade in Student

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,15 @@
 public class Student implements Comparable<Student> {
     public final String name;
     public final int id;
+    public final int grade; 
 
-    public Student(String name, int id) {
+    public Student(String name, int id, int grade) {
         this.name =  name;
         this.id = id;
+        this.grade = grade; 
     }
-
+    
+    @Override 
     public int compareTo(Student student) {
         if (this.id < student.id) {
             return -1;
