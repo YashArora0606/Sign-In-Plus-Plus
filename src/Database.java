de344ef6834ef79d4d0f6248b5b8efa944711b15
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-17 20:50:22 -0500

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,18 +1,16 @@
-import java.io.IOException;
-
 /**
  * Database (very underdeveloped)
  */
 public class Database {
 
-    // we should replace these with our own data structures later
     private final String[] reasons; //different reasons for sign in
     private final String[] subjects; //different subjects for sign in
+
     private ExcelManager master;
 
     public Database() {
-    	
-    	master = new ExcelManager("MasterList.xlsx");
+
+        master = new ExcelManager("MasterList.xlsx");
 
         //instead of hardcoding reasons or subjects, we could read from a .txt file to make it more dynamic
         reasons = new String[] {
@@ -24,6 +22,7 @@ public class Database {
                 "Physical Ed.", "Technology Studies", "Social Sciences", "Lunch / Spare"
         };
     }
+    
 
     public String[] getReasons() {
         return reasons;
@@ -33,43 +32,33 @@ public class Database {
         return subjects;
     }
 
-    /**
-     * Empty sign in method
-     * Should throw some errors later to be fancy (so we can display the message)
-     * @param id
-     * @param course
-     * @param reason
-     * @return true if the signing was successful
-     */
-    public boolean signIn(String id, String course, String reason) {
-    	
-    	// if it is not a student number
-    	// return false
-    	// else
-    	// sign in
-    	// return true
-    	master.signStudentIn(id, course, reason);
+    
+    public boolean signIn(String id, String course, String reason) throws InvalidIdException {
+        if (!validateId(id)) { //validate id
+            throw new InvalidIdException(id);
+        }
+
+        master.signStudentIn(id, course, reason);
+        return true;
+    }
+    
+    public boolean signOut(String id) throws InvalidIdException{
+        if (!validateId(id)) { //validate id
+            throw new InvalidIdException(id);
+        }
+
+        master.signStudentOut(id);
         return true;
     }
 
-    /**
-     * Empty sign out method
-     * Should throw some errors later too
-     * @param id
-     * @return true if the signing out was successful
-     */
-    public boolean signOut(String id) {
-        return master.signStudentOut(id);
+    public void close() {
+        master.closeExcelFile();
     }
 
-	public void close() {
-		try {
-			master.closeExcelFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-    
-
+    private boolean validateId(String id) {
+        if (id == null || id.length() != 9  || !Utils.isAnInteger(id)) {
+            return false;
+        }
+        return true;
+    }
 }
