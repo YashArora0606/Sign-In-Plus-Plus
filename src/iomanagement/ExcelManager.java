0166fe04484ff6f032e7b126c9912c50aef29225
Yash Arora <yash_arora6@hotmail.com>
2018-11-22 11:04:03 -0500

    boi

--- src/iomanagement/ExcelManager.java
+++ src/iomanagement/ExcelManager.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ExcelManager {
 
@@ -20,6 +21,8 @@ public class ExcelManager {
 	private CellStyle stringStyle;
 	private CellStyle dateStyle;
 	private CellStyle timeStyle;
+	
+	private ArrayList<XSSFSheet> reasonLists = new ArrayList<XSSFSheet>();
 
 	private final String[] header = {
 		"Student Number", "First Name", "Last Name", "Date", "Sign-In Time", "Sign-Out Time", "Teacher", "Reason"
@@ -33,7 +36,10 @@ public class ExcelManager {
 
 			FileInputStream fs = new FileInputStream(file);
 			workbook = new XSSFWorkbook(fs);
-			spreadsheet = workbook.getSheet("SignInSheet");
+			spreadsheet = workbook.createSheet("Master");
+			
+			
+			
 
             dateStyle = workbook.createCellStyle();
             dateStyle.setDataFormat((short)14);
@@ -66,21 +72,30 @@ public class ExcelManager {
         }
 
         newRow.getCell(0).setCellValue(session.student.id);
+        
         newRow.getCell(1).setCellValue(session.student.firstName);
+        
         newRow.getCell(2).setCellValue(session.student.lastName);
+        
         newRow.getCell(3).setCellStyle(dateStyle);
         newRow.getCell(3).setCellValue(session.getStartTime());
+        
         newRow.getCell(4).setCellStyle(timeStyle);
         newRow.getCell(4).setCellValue(session.getStartTime());
+        
         newRow.getCell(5).setCellStyle(timeStyle);
         newRow.getCell(5).setCellValue(session.getEndTime());
+        
         newRow.getCell(6).setCellValue(session.course);
+        
         newRow.getCell(7).setCellValue(session.reason);
     }
 
 
 	public void close() {
+		
 		try {
+			
 			FileOutputStream out = new FileOutputStream(file);
 			workbook.write(out);
 			out.close();
@@ -89,6 +104,7 @@ public class ExcelManager {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		System.out.println(file.getName() + " written successfully");
 	}
 }
