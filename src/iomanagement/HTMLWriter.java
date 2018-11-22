6c254967f2821e5802f6912ac29e5810a51ff33b
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-11-22 10:32:08 -0500

    made panels and changed some css

---
+++ src/iomanagement/HTMLWriter.java
@@ -0,0 +1,20 @@
+package iomanagement;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import datamanagment.Session;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class HTMLWriter {
+    HTMLWriter(String excelFile){
+
+    }
+}
