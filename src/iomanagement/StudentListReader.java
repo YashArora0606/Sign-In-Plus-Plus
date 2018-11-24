26e1925c465bb03f668b815bfb855dea50d85351
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-24 17:03:59 -0500

    Student List Reader

--- src/iomanagement/StudentListReader.java
+++ src/iomanagement/StudentListReader.java
@@ -1,10 +1,16 @@
 package iomanagement;
 
 import datamanagment.Student;
+import display.ErrorWindow;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,31 +20,51 @@ import java.util.Arrays;
  */
 public class StudentListReader {
 
+    private final String STUDENTS_DIR = "database/";
+    private final String STUDENTS_FILE = "Students.xlsx";
+    private final String[] header = {"Studnet ID", "First Name", "Last Name", "Grade"};
+
     private ArrayList<Student> students = new ArrayList<>();
 
-    public StudentListReader(String fileName) {
+    public StudentListReader() {
 
         try {
-            FileInputStream inputStream = new FileInputStream(fileName);
+            FileInputStream inputStream = new FileInputStream(STUDENTS_DIR + STUDENTS_FILE);
 
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
             XSSFSheet spreadsheet = workbook.getSheetAt(0);
 
             for (int row = 1; row < spreadsheet.getLastRowNum() + 1; row++) {
-                String id = formatId(spreadsheet.getRow(row).getCell(0).getRawValue());
-                String firstName = formatId(spreadsheet.getRow(row).getCell(1).toString());
-                String lastName = formatId(spreadsheet.getRow(row).getCell(2).toString());
-                String grade = formatId(spreadsheet.getRow(row).getCell(3).toString());
+                String id = spreadsheet.getRow(row).getCell(0).getRawValue();
+                String firstName = spreadsheet.getRow(row).getCell(1).toString();
+                String lastName = spreadsheet.getRow(row).getCell(2).toString();
+                String grade = spreadsheet.getRow(row).getCell(3).toString();
                 students.add(new Student(firstName, lastName, id, grade));
             }
 
             inputStream.close();
 
+        // What happens when a file cannot be found
+        } catch (FileNotFoundException e) {
+
+            createNewStudentFile();
+
+            new ErrorWindow("File containing Students could not be found");
+
+            try {
+                Desktop desktop = Desktop.getDesktop();
+                desktop.open(new File(STUDENTS_DIR));
+            } catch (IOException b) {
+                b.printStackTrace();
+            }
+
+            System.exit(1);
+
+            // What happens when an unknown error occurs
         } catch (IOException e) {
-            e.printStackTrace();
+
         }
     }
-
 
     public Student[] getStudents() {
         Student[] studentArray = students.toArray(new Student[0]);
@@ -46,18 +72,25 @@ public class StudentListReader {
         return studentArray;
     }
 
-    private String formatId(String id) {
-        for (int i = id.length(); i < 9; i++) {
-            id = "0" + id;
+    private void createNewStudentFile() {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Student List");
+
+        XSSFRow row = sheet.createRow(0);
+        for (int i = 0; i < header.length; i++) {
+            row.createCell(i).setCellValue(header[i]);
+            sheet.autoSizeColumn(i);
         }
-        return id;
+
+        try {
+            File file = new File(STUDENTS_DIR + STUDENTS_FILE);
+            file.createNewFile();
+            FileOutputStream out = new FileOutputStream(file);
+            workbook.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private String formatName(String name) {
-        return name;
-    }
-
-    private String formatGrade(String grade) {
-        return grade;
-    }
 }
