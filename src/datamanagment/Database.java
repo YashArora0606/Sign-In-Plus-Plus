61f63d62b783af256797ae12fe8621e61c452a03
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-23 22:24:25 -0500

    Buncha Removals

--- src/datamanagment/Database.java
+++ src/datamanagment/Database.java
@@ -1,13 +1,12 @@
 package datamanagment;
-import java.util.Arrays;
-import java.util.LinkedList;
 
 import exceptions.AlreadyLoggedInException;
 import exceptions.InvalidIdException;
 import exceptions.NotLoggedInException;
-import iomanagement.ExcelManager;
 import iomanagement.StudentListReader;
 import utilities.Utils;
+
+import java.util.Arrays;
 
 /**
  * Database (very underdeveloped)
@@ -23,17 +22,11 @@ public class Database {
             "Physical Ed.", "Technology Studies", "Social Sciences", "Lunch / Spare"
     };
 
-    private ExcelManager master;
-
     //replace with data structures later
     private Student[] students;
-    private LinkedList<Session> sessionsToResolve = new LinkedList<>();
 
     public Database() {
-
-        master = new ExcelManager("MasterList.xlsx");
-
-        students = new StudentListReader("StudentList.xlsx").getStudents();
+        students = new StudentListReader("database/StudentList.xlsx").getStudents();
         Arrays.sort(students);
     }
 
@@ -51,12 +44,6 @@ public class Database {
             throw new InvalidIdException(id);
         }
 
-        Session session = findSession(student);
-        if (session != null) {
-            throw new AlreadyLoggedInException();
-        }
-
-        sessionsToResolve.add(new Session(student, courseMissed, reason, course));
         return true;
     }
 
@@ -67,36 +54,18 @@ public class Database {
             throw new InvalidIdException(id);
         }
 
-        Session session = findSession(student);
-        if (session == null) {
-            throw new NotLoggedInException();
-        }
-
-        session.resolve();
-
-        master.logSession(session);
 
         return true;
     }
 
-    /**
-     * reconcileData()
-     * @param unknown
-     * @return boolean whether or not the data was successfully able to be reconciled or not
-     */
+
     public boolean reconcileData() {
 
         return true;
     }
 
-    /**
-     * close()
-     * Method that closes the document in the ExcelManager ensuring that it is not left open to data leaks
-     */
     public void close() {
-        master.close();
     }
-
 
     private Student findStudent(String id) {
         if (id == null || id.length() != 9 || !Utils.isAnInteger(id)) {
@@ -118,15 +87,6 @@ public class Database {
             }
         }
 
-        return null;
-    }
-
-    private Session findSession(Student student) {
-        for (Session session : sessionsToResolve) {
-            if (session.student.equals(student)) {
-                return session;
-            }
-        }
         return null;
     }
 }
--- src/iomanagement/ExcelManager.java
+++ src/iomanagement/ExcelManager.java
@@ -1,163 +1,32 @@
 package iomanagement;
-import org.apache.poi.ss.usermodel.CellStyle; 
-import org.apache.poi.xssf.usermodel.XSSFRow;
+
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import datamanagment.Session;
-import utilities.Utils;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class ExcelManager {
+
+    private static final ExcelManager DEFAULT_INSTANCE = new ExcelManager();
+
+    public static ExcelManager getInstance() {
+        return DEFAULT_INSTANCE;
+    }
 
     private File file;
 	private XSSFWorkbook workbook;
 	private XSSFSheet master;
 
-	private CellStyle stringStyle;
 	private CellStyle dateStyle;
 	private CellStyle timeStyle;
-	
-	private ArrayList<XSSFSheet> reasonSheets = new ArrayList<XSSFSheet>();
 
 	private final String[] header = {
-		"Student Number", "First Name", "Last Name", "Date", "Sign-In Time", "Sign-Out Time", "Teacher", "Reason", "Subject Missed"
+		"Student Number", "First Name", "Last Name", "Date", "Sign-In Time", "Sign-Out Time", "Reason", "Subject Work", "Subject Missed"
 	};
-	
-	private static String[] reasons = Utils.getReasons();
 
-
-	public ExcelManager(String fileName) {
-		try {
-			file = new File(fileName);
-			file.createNewFile();
-
-			FileInputStream fs = new FileInputStream(file);
-			workbook = new XSSFWorkbook(fs);
-			master = workbook.getSheet("Master");
-            initializeSpreadsheets(master);
-
-			for (int i = 0; i < reasons.length; i++) {
-				reasonSheets.add(workbook.getSheet(reasons[i]));
-	            initializeSpreadsheets(workbook.getSheet(reasons[i]));
-			}
-
-            dateStyle = workbook.createCellStyle();
-            dateStyle.setDataFormat((short) 14);
-
-            timeStyle = workbook.createCellStyle();
-            timeStyle.setDataFormat(workbook.getCreationHelper().createDataFormat().getFormat("h:mm"));
-
-
-            fs.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public ExcelManager() {
 	}
 
-	private void initializeSpreadsheets(XSSFSheet sheet) {
-		XSSFRow titleRow = sheet.createRow(0);
-		for (int i = 0; i < header.length; i++) {
-            titleRow.createCell(i).setCellValue(header[i]);
-        }
-	}
 
-	public void logSession(Session session) {
-        master.shiftRows(1, Math.max(1, master.getLastRowNum()), 1);
-
-        XSSFRow newRow = master.createRow(1);
-        for (int i = 0; i< header.length; i++) {
-            newRow.createCell(i);
-        }
-
-        newRow.getCell(0).setCellValue(session.student.id);
-        
-        newRow.getCell(1).setCellValue(session.student.firstName);
-        
-        newRow.getCell(2).setCellValue(session.student.lastName);
-        
-        newRow.getCell(3).setCellStyle(dateStyle);
-        newRow.getCell(3).setCellValue(session.getStartTime());
-        
-        newRow.getCell(4).setCellStyle(timeStyle);
-        newRow.getCell(4).setCellValue(session.getStartTime());
-        
-        newRow.getCell(5).setCellStyle(timeStyle);
-        newRow.getCell(5).setCellValue(session.getEndTime());
-        
-        newRow.getCell(6).setCellValue(session.subjectWork);
-        
-        newRow.getCell(7).setCellValue(session.reason);
-        
-        newRow.getCell(8).setCellValue(session.courseMissed);
-
-        
-        
-        logSessionSubsection(session);
-    }
-	
-	public void logSessionSubsection(Session session) {
-		
-		int workingIndex = -1;
-		
-		for(int i = 0; i < reasons.length; i++) {
-			if (session.reason.equals(reasons[i])) {
-				workingIndex = i;
-			}
-		}
-		
-		
-		
-		reasonSheets.get(workingIndex).shiftRows(1, Math.max(1, reasonSheets.get(workingIndex).getLastRowNum()), 1);
-
-        XSSFRow newRow = reasonSheets.get(workingIndex).createRow(1);
-        for (int i = 0; i< header.length; i++) {
-            newRow.createCell(i);
-        }
-
-        newRow.getCell(0).setCellValue(session.student.id);
-        
-        newRow.getCell(1).setCellValue(session.student.firstName);
-        
-        newRow.getCell(2).setCellValue(session.student.lastName);
-        
-        newRow.getCell(3).setCellStyle(dateStyle);
-        newRow.getCell(3).setCellValue(session.getStartTime());
-        
-        newRow.getCell(4).setCellStyle(timeStyle);
-        newRow.getCell(4).setCellValue(session.getStartTime());
-        
-        newRow.getCell(5).setCellStyle(timeStyle);
-        newRow.getCell(5).setCellValue(session.getEndTime());
-        
-        newRow.getCell(6).setCellValue(session.subjectWork);
-        
-        newRow.getCell(7).setCellValue(session.reason);
-        
-        newRow.getCell(8).setCellValue(session.courseMissed);
-
-    }
-
-
-	public void close() {
-		
-		try {
-			
-			FileOutputStream out = new FileOutputStream(file);
-			workbook.write(out);
-			out.close();
-
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println(file.getName() + " written successfully");
-	}
 }
