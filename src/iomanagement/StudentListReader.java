b6906dfc94a02aa4681310a4be7978adc822496a
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-25 05:21:11 -0500

    Edited Some Try-Catching

--- src/iomanagement/StudentListReader.java
+++ src/iomanagement/StudentListReader.java
@@ -43,80 +43,80 @@ public class StudentListReader {
      */
     public StudentListReader() {
 
+        XSSFWorkbook workbook = null;
+
         try {
+            workbook = ExcelUtils.loadFile(STUDENTS_DIR + STUDENTS_FILE);
 
-            //load the .xlsx file
-            XSSFWorkbook workbook = ExcelUtils.loadFile(STUDENTS_DIR + STUDENTS_FILE);
-
-            //open first sheet (by default, an excel file must contain at least 1 sheet)
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
-            //create cell styles
-            XSSFCellStyle redCell = workbook.createCellStyle();
-            redCell.setFillForegroundColor(IndexedColors.RED.getIndex());
-            redCell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-            XSSFCellStyle defaultCell = workbook.createCellStyle();
-            defaultCell.setFillPattern(FillPatternType.NO_FILL);
-
-            //read file
-            Queue<Cell> flaggedCells = new SinglyLinkedList<>(); //queue of cells that are improperly formatted
-            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-
-                XSSFRow row = sheet.getRow(rowIndex);
-                if (row == null) {
-                    row = sheet.createRow (rowIndex);
-                }
-
-                String[] fields = new String[4];
-                for (int col = 0; col < fields.length; col++) {
-                    XSSFCell cell = row.getCell(col);
-                    if (cell == null) {
-                        cell = row.createCell(col);
-                    }
-
-                    cell.setCellStyle(defaultCell);
-
-                    String value = ExcelUtils.getValueFromCell(cell).trim();
-                    if (!isValidField(value, col)) {
-                        flaggedCells.add(cell);
-                    }
-                    fields[col] = value;
-                }
-
-                students.add(new Student(fields[0], fields[1], fields[2], fields[3]));
-            }
-
-            if (flaggedCells.size() > 0) {
-                while (flaggedCells.size() > 0) {
-                    flaggedCells.poll().setCellStyle(redCell);
-                }
-
-                ExcelUtils.saveFile(workbook, STUDENTS_DIR + STUDENTS_FILE);
-
-                throw new FileFormatException();
-            }
-
-            ExcelUtils.saveFile(workbook, STUDENTS_DIR + STUDENTS_FILE);
-
-            // What happens when a file cannot be found
         } catch (FileNotFoundException e) {
             new ErrorWindow("Students.xlsx could not be found and was auto-generated");
             createNewStudentFile();
             openDirectory();
             System.exit(1);
 
-        // Improperly formatted excel document
-        } catch (FileFormatException e) {
-            new ErrorWindow("Students.xlsx not properly formatted");
-            openDirectory();
-            System.exit(1);
-
-            // What happens when an unknown error occurs
         } catch (IOException e) {
             new ErrorWindow("Unknown error in Students.xlsx");
             System.exit(1);
+        }
 
+        //open first sheet (by default, an excel file must contain at least 1 sheet
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        //create cell styles
+        XSSFCellStyle redCell = workbook.createCellStyle();
+        redCell.setFillForegroundColor(IndexedColors.RED.getIndex());
+        redCell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFCellStyle defaultCell = workbook.createCellStyle();
+        defaultCell.setFillPattern(FillPatternType.NO_FILL);
+
+        //read file
+        boolean fileProperlyFomatted = true;
+        Queue<String[]> fieldQueue = new SinglyLinkedList<>();
+
+        for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+
+            XSSFRow row = sheet.getRow(rowIndex);
+            if (row == null) {
+                row = sheet.createRow(rowIndex);
+            }
+
+            String[] fields = new String[4];
+            for (int col = 0; col < fields.length; col++) {
+                XSSFCell cell = row.getCell(col);
+                if (cell == null) {
+                    cell = row.createCell(col);
+                }
+
+                cell.setCellStyle(defaultCell);
+
+                String value = ExcelUtils.getValueFromCell(cell).trim();
+                if (!isValidField(value, col)) {
+                    fileProperlyFomatted = false;
+                    cell.setCellStyle(redCell);
+                }
+                fields[col] = null;
+            }
+
+            fieldQueue.add(fields);
+        }
+
+        try {
+            ExcelUtils.saveFile(workbook, STUDENTS_DIR + STUDENTS_FILE);
+        } catch (IOException e) {
+            new ErrorWindow("Unknown error in Students.xlsx");
+            System.exit(1);
+        }
+
+        if (!fileProperlyFomatted) {
+            new ErrorWindow("Students.xlsx improperly formatted");
+            openDirectory();
+            System.exit(1);
+        }
+
+        while (fieldQueue.size() > 0) {
+            String[] fields = fieldQueue.poll();
+            students.add(new Student(fields[0], fields[1], fields[2], fields[3]));
         }
     }
 
