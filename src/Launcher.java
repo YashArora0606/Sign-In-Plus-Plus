1c0f227c6162622e81c0d071a38024139672ac15
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-11-28 02:07:54 -0500

    Removing debug code

--- src/Launcher.java
+++ src/Launcher.java
@@ -19,17 +19,5 @@ public class Launcher {
         Database database = new DerbyDatabase();
         SignInManager signInManager = new SignInManager(database);
         Window frame = new Window(signInManager);
-
-        try {
-            System.out.println(signInManager.addStudent(74264672, "Alston", "Lo", 12));
-        } catch (StudentAlreadyExistsException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            System.out.println(signInManager.removeStudent(74264672));
-        } catch (StudentDoesNotExistException e) {
-            e.printStackTrace();
-        }
     }
 }
