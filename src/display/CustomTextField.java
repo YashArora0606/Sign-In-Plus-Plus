d6546d17408fa22b04829e7d711a10a63ee677e6
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-11-30 11:08:34 -0500

    mroe fixe

--- src/display/CustomTextField.java
+++ src/display/CustomTextField.java
@@ -56,7 +56,7 @@ public class CustomTextField extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		titleButton = new CustomButton(title, getPreferredSize().width/2 - 220/2, 0, Utils.scale(220), Utils.scale(30), Utils.colours[3]);
+		titleButton = new CustomButton(title, getPreferredSize().width/2 - Utils.scale(220)/2, 0, Utils.scale(220), Utils.scale(30), Utils.colours[3]);
 		titleButton.setSelectable(false);
 		titleButton.draw(g, this);
 		
