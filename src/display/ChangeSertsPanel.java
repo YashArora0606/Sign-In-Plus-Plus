fe335528beff4a8767eddee9302c7efb61d0ae5b
Yash Arora <yash_arora6@hotmail.com>
2018-12-04 09:46:18 -0500

    more shit

--- src/display/ChangeSertsPanel.java
+++ src/display/ChangeSertsPanel.java
@@ -31,89 +31,93 @@ import java.util.Scanner;
 
 import utilities.Utils;
 
-public class ChangeSertsPanel extends JPanel{
-    private Window display;
-    private JPanel panel;
-    private int maxX;
-    private int maxY;
-    private CustomButton back;
-    private CustomButton submit;
-    AddAndRemoveMenu a;
-    
-    String message = "SERTS will be updated automatically when you close the program.";
-    String menuMessage = "";
-    
-    JLayeredPane pane;
+public class ChangeSertsPanel extends JPanel {
+	private Window display;
+	private JPanel panel;
+	private int maxX;
+	private int maxY;
+	private CustomButton back;
+	private CustomButton submit;
+	AddAndRemoveMenu a;
 
-    /**
-     * Constructor
-     * @param display the Window object to which this panel belongs
-     */
-    ChangeSertsPanel(Window display) {
-        this.display = display;
-        this.panel = this;
-        this.maxX = display.maxX;
-        this.maxY = display.maxY;
-        
-        setBackground(Utils.colours[1]);
-        
-        this.addMouseListener(new MyMouseListener());
-        
+	String message = "SERTS will be updated automatically when you close the program.";
+	String menuMessage = "";
+
+	JLayeredPane pane;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param display the Window object to which this panel belongs
+	 */
+	ChangeSertsPanel(Window display) {
+		this.display = display;
+		this.panel = this;
+		this.maxX = display.maxX;
+		this.maxY = display.maxY;
+
+		setBackground(Utils.colours[1]);
+
+		this.addMouseListener(new MyMouseListener());
+
 		pane = new JLayeredPane();
-		
-        
-        a = new AddAndRemoveMenu(SignInManager.getSertList(), "SERT", maxY);
+
+		a = new AddAndRemoveMenu(SignInManager.getSertList(), "SERT", maxY);
 //        a.setBounds(a.getBounds().x, Utils.scale(100), a.getBounds().width, a.getBounds().height);
-        
-        JPanel emptyTop = new JPanel();
-        emptyTop.setPreferredSize(new Dimension(maxX, Utils.scale(200)));
-        emptyTop.setOpaque(false);
-        add(emptyTop, BorderLayout.NORTH);
-        add(a, BorderLayout.SOUTH);
-        	
-    }
 
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
+		JPanel emptyTop = new JPanel();
+		emptyTop.setPreferredSize(new Dimension(maxX, Utils.scale(200)));
+		emptyTop.setOpaque(false);
+		add(emptyTop, BorderLayout.NORTH);
+		add(a, BorderLayout.SOUTH);
 
-        //back button leads to the home panel
-        back = new CustomButton("Back",0,0, Utils.scale(115), Utils.scale(80), Utils.colours[3]);
-        back.draw(g, panel);
+	}
 
-        
-        CustomButton messageButton = new CustomButton(message, maxX/2, Utils.scale(40), Utils.scale(0), Utils.scale(80), null);
-        messageButton.draw(g, panel);
-        
-        menuMessage = a.getMessage();
-        CustomButton menuMessageButton = new CustomButton(menuMessage, maxX/2, Utils.scale(100), Utils.scale(0), Utils.scale(80), null);
-        menuMessageButton.draw(g, panel);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
-        
-        repaint();
-    }
-    
-    private class MyMouseListener implements MouseListener {
-        public void mouseEntered(MouseEvent e){
+		// back button leads to the home panel
+		back = new CustomButton("Back", 0, 0, Utils.scale(115), Utils.scale(80), Utils.colours[3]);
+		back.draw(g, panel);
 
-        }
+		CustomButton messageButton = new CustomButton(message, maxX / 2, Utils.scale(40), Utils.scale(0),
+				Utils.scale(80), null);
+		messageButton.draw(g, panel);
 
-        /**
-         * Checks which button was clicked and responds accordingly
-         * @param e the mouse event which occurred
-         */
-        public void mouseClicked(MouseEvent e) {
-        	if (back.isMouseOnButton(panel)) {
-        		display.changeState(5);
-        	}
-        }
-        public void mousePressed(MouseEvent e){
+		menuMessage = a.getMessage();
+		CustomButton menuMessageButton = new CustomButton(menuMessage, maxX / 2, Utils.scale(100), Utils.scale(0),
+				Utils.scale(80), null);
+		menuMessageButton.draw(g, panel);
 
-        }
-        public void mouseExited(MouseEvent e){
+		repaint();
+	}
 
-        }
-        public void mouseReleased(MouseEvent e){
+	private class MyMouseListener implements MouseListener {
+		public void mouseEntered(MouseEvent e) {
 
-        }
-    }
+		}
+
+		/**
+		 * Checks which button was clicked and responds accordingly
+		 * 
+		 * @param e the mouse event which occurred
+		 */
+		public void mouseClicked(MouseEvent e) {
+			if (back.isMouseOnButton(panel)) {
+				display.changeState(5);
+			}
+		}
+
+		public void mousePressed(MouseEvent e) {
+
+		}
+
+		public void mouseExited(MouseEvent e) {
+
+		}
+
+		public void mouseReleased(MouseEvent e) {
+
+		}
+	}
 }
